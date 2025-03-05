6712149edb189f72ee889a0a6f13c38ac989b33b
Fernando <fer.eze.ram@gmail.com>
2025-03-05 01:23:43 -0300

    Add asLabel prop to Question component

--- src/components/messages/question.tsx
+++ src/components/messages/question.tsx
@@ -3,15 +3,34 @@ import {cn} from "@/helpers/cn";
 interface QuestionProps {
   className?: string;
   children?: React.ReactNode;
+  id?: string;
+  asLabel?: boolean;
+  htmlFor?: string;
 }
 
-export function Question({children, className}: QuestionProps) {
+export function Question({children, className, id, asLabel = false, htmlFor}: QuestionProps) {
+  if (asLabel) {
+    return (
+      <label
+        className={cn(
+          "w-full text-left text-base font-semibold text-balance supports-moz-none:font-medium sm:text-xl",
+          className,
+        )}
+        htmlFor={htmlFor}
+        id={id}
+      >
+        {children}
+      </label>
+    );
+  }
+
   return (
     <legend
       className={cn(
         "w-full text-left text-base font-semibold text-balance supports-moz-none:font-medium sm:text-xl",
         className,
       )}
+      id={id}
     >
       {children}
     </legend>
