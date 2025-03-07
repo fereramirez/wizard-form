734dff9dd26e3af41d5c93d810340ad60c49d6a7
Fernando <fer.eze.ram@gmail.com>
2025-03-07 15:18:37 -0300

    Update texts

--- src/components/funnel-steps/wait.tsx
+++ src/components/funnel-steps/wait.tsx
@@ -10,7 +10,7 @@ export function WaitForPromise() {
       <div className="flex grow flex-col items-center justify-evenly gap-8">
         <Title>
           Remember for the data we would use in the last step? If you see this step, it means that
-          the data is not ready yet
+          the data is not ready yet. Please wait.
         </Title>
 
         <span className="flex max-h-72 w-full max-w-72 grow items-center justify-center">
@@ -25,9 +25,13 @@ export function WaitStep() {
   return (
     <InOutAnimationWrapper>
       <div className="flex grow flex-col items-center justify-evenly gap-8">
-        <Title>This step is used to wait for data to be ready if needed</Title>
+        <Title>
+          We can render different steps based on the data collected, the next step will be chosen
+          based on the <strong className="text-primary-1">random number</strong> generated a few
+          steps before.
+        </Title>
 
-        <Countdown initialTime={6} />
+        <Countdown initialTime={10} />
 
         <span className="flex max-h-72 w-full max-w-72 grow items-center justify-center">
           <Spinner className="size-full grow animate-spin" fill="currentColor" />
