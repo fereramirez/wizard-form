f9c412d1156a12e144e81a763367ac35f5cc5ae5
Fernando <fer.eze.ram@gmail.com>
2025-03-08 21:32:55 -0300

    Add comment

--- src/components/messages/data-collected.tsx
+++ src/components/messages/data-collected.tsx
@@ -35,3 +35,26 @@ export function DataCollected({children, className, keyName, value}: DataCollect
     </section>
   );
 }
+
+/*
+
+<table
+  className={cn(
+    fragmentMono.className,
+    "grow overflow-x-hidden overflow-y-auto rounded-xs bg-white text-black [&_td]:px-2 [&_th]:px-2",
+    className,
+  )}
+>
+  {keyName && value ? (
+    <thead>
+      <tr className="border-primary-3 sticky top-0 grid auto-rows-fr grid-cols-2 gap-2 border-b bg-black py-1 text-left text-white">
+        <th>{keyName}</th>
+        <th>{value}</th>
+      </tr>
+    </thead>
+  ) : null}
+
+  <tbody>{children}</tbody>
+</table>
+
+*/
