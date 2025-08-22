38456c75123bb8e4ed289e641e6d8d133bddfdad
Fernando <fer.eze.ram@gmail.com>
2025-08-22 19:20:09 -0300

    Add Suspense to avoid useSearchParam error when deploying

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+import {Suspense} from "react";
+
 import {FunnelCard} from "@/components/funnel/funnel-card";
 import {PageWrapper} from "@/components/wrappers/page-wrapper";
 
 export default async function HomePage() {
   return (
     <PageWrapper>
-      <FunnelCard />
+      <Suspense>
+        <FunnelCard />
+      </Suspense>
     </PageWrapper>
   );
 }
